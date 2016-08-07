c953a41b40fffed853b5432b248ab589e11f4fae
sudodoki <smd.deluzion@gmail.com>
2016-08-07 18:16:09 +0400

    Add findSubtree, createSubtree WIP

--- test/utils/trees.js
+++ test/utils/trees.js
@@ -4,28 +4,27 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 const trees = require('../../lib/utils/trees');
 describe('utils/trees', () => {
+  const rootNode = { path: './' };
+  const node1 = {
+    path: 'file.txt',
+    content: '2ahash',
+  };
+  const node2 = {
+    path: 'someFolder',
+  };
+  const node3 = {
+    path: 'someFile',
+    content: 'anotherhash',
+  };
+  const sampleTree = Object.assign({}, rootNode, {
+    children: [
+      Object.assign({}, node1),
+      Object.assign({}, node2, {
+        children: [Object.assign({}, node3)],
+      }),
+    ],
+  });
   describe('#bfs', () => {
-    const rootNode = { path: './' };
-    const node1 = {
-      path: 'file.txt',
-      content: '2ahash',
-    };
-    const node2 = {
-      path: 'someFolder',
-    };
-    const node3 = {
-      path: 'someFile',
-      content: 'anotherhash',
-    };
-    const sampleTree = Object.assign({}, rootNode, {
-      children: [
-        Object.assign({}, node1),
-        Object.assign({}, node2, {
-          children: [Object.assign({}, node3)],
-        }),
-      ],
-    });
-
     it('should be defined', () => {
       expect(trees.bfs).to.be.ok;
     });
@@ -59,5 +58,44 @@ describe('utils/trees', () => {
     });
 
   });
-  xdescribe('something else', () => {});
+
+  describe('#findSubtree', () => {
+    it('should be defined', () => {
+      expect(trees.findSubtree).to.be.ok;
+    });
+    it('should accept tree and path', () => {
+      expect(trees.findSubtree.bind(null, sampleTree, [])).not.to.throw;
+    });
+    it('should return matching node if present (shallow)', () => {
+      const result = trees.findSubtree(sampleTree, [rootNode.path, node1.path]);
+      expect(result).to.eql(node1);
+    });
+    it('should return matching node if present (nested)', () => {
+      const result = trees.findSubtree(sampleTree, [rootNode.path, node2.path, node3.path]);
+      expect(result).to.eql(node3);
+    });
+    it('should return matching subtree if present (nested)', () => {
+      const result = trees.findSubtree(sampleTree, [rootNode.path, node2.path]);
+      expect(result).to.eql(Object.assign({}, node2, {
+        children: [Object.assign({}, node3)],
+      }));
+    });
+    it('should return null for non-existent path', () => {
+      const result = trees.findSubtree(sampleTree, [node2.path]);
+      expect(result).to.equal(null);
+    });
+    it('should return null for empty trees', () => {
+      const result = trees.findSubtree({ path: './', children: [] }, [rootNode.path, node2.path]);
+      expect(result).to.equal(null);
+    });
+  });
+
+  describe('#createSubtree', () => {
+    it('should be defined', () => {
+      expect(trees.createSubtree).to.be.ok;
+    });
+    it('should accept tree and path', () => {
+      expect(trees.createSubtree.bind(null, sampleTree, ['./', 'someNewFolder', 'someNewFile'])).not.to.throw;
+    });
+  });
 });
